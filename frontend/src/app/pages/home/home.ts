bc0b91f7f1583848b5b31a35e4d1429f6181df60
Anass Lazaar <anasslazaar9@gmail.com>
2025-12-08 04:02:21 +0100

    Seperating suggested-user from home into a single component and used in home and post page

--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -7,12 +7,13 @@ import { PostResponse } from '../../models/global.model';
 import { PostOptionsMenuComponent } from '../../share/PostOptionsMenu/post-options-menu';
 import { ConfirmDialogComponent } from '../../share/ConfirmDialogComponent/confirm-dialog';
 import { UserService } from '../../services/UserService';
+import { SuggestedUsersComponent } from '../../share/SuggestedAccounts/suggested-users';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   // Removed AddPost from here
-  imports: [CommonModule, PostOptionsMenuComponent, ConfirmDialogComponent, RouterLink],
+  imports: [CommonModule, PostOptionsMenuComponent, ConfirmDialogComponent, RouterLink, SuggestedUsersComponent],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
@@ -38,47 +39,9 @@ export class Home implements OnInit {
     this.isAdmin = this.tokenService.isAdmin();
     this.currentUserId = this.tokenService.getUUID();
     this.loadPosts();
-    if (!this.isAdmin) {
-      this.loadSuggestedUsers();
-    }
   }
 
-  toggleFollow(user: any) {
-    if (!this.currentUserId) {
-      this.router.navigate(['/login']);
-      return;
-    }
 
-    if (user.following) {
-      this.userService.unfollowUser(user.id).subscribe({
-        next: (res) => {
-          user.following = false;
-          console.log('Unfollowed ' + user.username);
-        },
-        error: (err) => {
-          console.error('ERROR UNFOLLOWING USER :', err);
-        },
-      });
-    } else {
-      this.userService.followUser(user.id).subscribe({
-        next: (res) => {
-          user.following = true;
-          console.log('Followed ' + user.username);
-        },
-        error: (err) => {
-          console.error('ERROR FOLLOWING USER :', err);
-        },
-      });
-    }
-  }
-
-  loadSuggestedUsers() {
-    this.userService.getSuggestedUsers().subscribe({
-      next: (data) => {
-        this.suggestedUsers = data;
-      },
-    });
-  }
 
   loadPosts() {
     this.loading = true;
